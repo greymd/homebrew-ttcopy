83ad30920ad5a4d07bb6d4919a760330caad20d9
greymd <greengregson@gmail.com>
2017-03-14 13:42:03 +0900

    Bump version to 2.2.1

--- ttcopy.rb
+++ ttcopy.rb
@@ -1,8 +1,8 @@
 class Ttcopy < Formula
   desc "Provide copying and pasting within multiple hosts through the Web."
   homepage "https://github.com/greymd/ttcopy.git"
-  url "https://github.com/greymd/ttcopy.git", :revision => "489f94b2fec7b804020bf6943895b0cd9770fa0a"
-  version "2.2.0"
+  url "https://github.com/greymd/ttcopy.git", :revision => "ea575683cce0116cd6d41b3569332f591a5c8724"
+  version "2.2.1"
   head "https://github.com/greymd/ttcopy.git", :branch => "master"
 
   def install
